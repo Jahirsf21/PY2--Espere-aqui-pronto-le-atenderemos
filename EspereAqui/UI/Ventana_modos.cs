cf8d7910e2a4d9a0ec93a1cec31000177e71f972
Jahirsf21 <deislhersanchezfunez@gmail.com>
2025-06-02 00:23:06 -0600

    Va con lo anterior.

---
+++ EspereAqui/UI/Ventana_modos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EspereAqui.UI
+{
+
+
+    public partial class Ventana_modos : Form
+    {
+        private Ventana_principal principal;
+        private Modo_auto auto;
+        private Ventana_manual manual;
+        public Ventana_modos(Ventana_principal principal)
+        {
+            InitializeComponent();
+            this.principal = principal;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Regresar
+        {
+            principal.Show();
+            principal.StartPosition = FormStartPosition.CenterScreen;
+            this.Hide();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e) // Manual
+        {
+            if (manual == null || manual.IsDisposed)
+            {
+                manual = new Ventana_manual(this);
+                manual.StartPosition = FormStartPosition.CenterScreen;
+            }
+            manual.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // auto
+        {
+            if (auto == null || auto.IsDisposed)
+            {
+                auto = new Modo_auto(this);
+                auto.StartPosition = FormStartPosition.CenterScreen;
+            }
+            auto.Show();
+            this.Hide();
+        }
+    }
+}
